0164abe59522ba46d36eed6767499f6de619c93b
Natsumi H <39241405+NatsumiHB@users.noreply.github.com>
2021-02-18 01:58:45 +0100

    Merge dev

--- src/Silk.Dashboard/Pages/Dashboard/Profile.razor.cs
+++ src/Silk.Dashboard/Pages/Dashboard/Profile.razor.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.Entities;
 using Microsoft.AspNetCore.Components;
 using Silk.Dashboard.Models.Discord;
+using Silk.Extensions;
 
 namespace Silk.Dashboard.Pages.Dashboard
 {
@@ -13,24 +16,36 @@ namespace Silk.Dashboard.Pages.Dashboard
         private string _token;
         private bool _oAuthTokenVisible;
 
-        private DiscordApiUser _user;
-        private List<DiscordApiGuild> AllGuilds { get; set; }
-        private List<DiscordApiGuild> OwnedGuilds { get; set; }
+        private bool _doneLoading;
+        private DiscordRestClient _client;
+        private IReadOnlyList<DiscordGuild> _allGuilds;
+        private IReadOnlyList<DiscordGuild> _ownedGuilds;
 
         private string OAuthTokenVisibility => _oAuthTokenVisible ? "text" : "password";
 
-        protected override async Task OnInitializedAsync()
+        protected override async Task OnAfterRenderAsync(bool firstRender)
         {
-            _user = await UserService.GetUserInfoAsync(HttpContextAccessor.HttpContext);
-            _token = await UserService.GetTokenAsync(HttpContextAccessor.HttpContext);
+            if(firstRender)
+            {
+                _token = await UserService.GetTokenAsync(HttpContextAccessor.HttpContext);
 
-            AllGuilds = await UserService.GetUserGuildsAsync(HttpContextAccessor.HttpContext);
-            OwnedGuilds = AllGuilds.Where(GuildIsOwnedByUser).ToList();
-        }
+                var config = new DiscordConfiguration
+                {
+                    Token = _token,
+                    TokenType = TokenType.Bearer
+                };
+                
+                _client = new DiscordRestClient(config); // This breaks things
 
-        private static bool GuildIsOwnedByUser(DiscordApiGuild guild)
-        {
-            return (guild.Permissions & DiscordManageServerPermission) == DiscordManageServerPermission;
+                _allGuilds = _client.Guilds.Values.ToList(); // await _client.GetCurrentUserGuildsAsync(100, 0, 0);
+                _ownedGuilds = _allGuilds.Where(g => 
+                    g.Members.First(m => m.Value.Id == _client.CurrentUser.Id)
+                        .Value.Roles.Any(r => r.HasPermission(Permissions.ManageGuild)))
+                    .ToList();
+
+                _doneLoading = true;
+                StateHasChanged();
+            }
         }
 
         private void ToggleOAuthTokenVisibility()
---
+++ src/Silk.Dashboard/Services/Concretions/DiscordRestClientService.cs
@@ -0,0 +1,26 @@
+﻿using AspNet.Security.OAuth.Discord;
+using DSharpPlus;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Silk.Dashboard.Services.Concretions
+{
+    public class DiscordRestClientService
+    {
+        public DiscordRestClient Client { get; set; }
+
+        public DiscordRestClientService(string token)
+        {
+            Client = new DiscordRestClient(new DiscordConfiguration
+            {
+                Token = token,
+                TokenType = TokenType.Bearer
+            });
+        }
+    }
+}
